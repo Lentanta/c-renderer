70b6c6d84aa7453ccf5977e506953b565b711f76
Thanh tam <lentanta@pop-os.localdomain>
2024-12-03 15:34:52 +0700

    Adding rotation to cube

--- Makefile
+++ Makefile
@@ -1,7 +1,11 @@
 build:
 	echo "Create dist folder and compile all C code"
 	mkdir dist
-	gcc -Wall -std=c99 ./src/*.c -lSDL2 -o ./dist/renderer
+	gcc -Wall -std=c99 ./src/*.c -lSDL2 -lm -o ./dist/renderer
+
+	# NOTE:
+	# -lm for linking the math lib
+	# -lSDL2 for linking the SDL2 lib
 
 run:
 	./dist/renderer
--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_keycode.h>
+#include <SDL2/SDL_timer.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -15,10 +16,12 @@ vec2_t projected_points[N_POINTS];
 // constants
 int WINDOW_WIDTH = 800;
 int WINDOW_HEIGHT = 600;
+int previous_frame_time = 0;
 
 bool is_running = false;
 float fov_factor = 600;
 vec3_t camera_position = {0, 0, -5};
+vec3_t cube_rotation = {0, 0, 0};
 
 void setup(void) {
   // Allocate the required memory to hold the color buffer
@@ -56,7 +59,16 @@ void process_input() {
     if (event.key.keysym.sym == SDLK_ESCAPE)
       is_running = false;
     if (event.key.keysym.sym == SDLK_RIGHT) {
-      fov_factor = fov_factor + 1;
+      camera_position.x += 0.1;
+    }
+    if (event.key.keysym.sym == SDLK_LEFT) {
+      camera_position.x -= 0.1;
+    }
+    if (event.key.keysym.sym == SDLK_UP) {
+      camera_position.z += 0.1;
+    }
+    if (event.key.keysym.sym == SDLK_DOWN) {
+      camera_position.z -= 0.1;
     }
     break;
   }
@@ -71,14 +83,33 @@ vec2_t project(vec3_t point) {
 }
 
 void update() {
+  // --- Lock begin: until the time passed (Locked fixed time step) --- //
+  int time_to_wait = FRAME_TARGET_TIME - (SDL_GetTicks() - previous_frame_time);
+  if (time_to_wait > 0 && time_to_wait <= FRAME_TARGET_TIME) {
+    SDL_Delay(time_to_wait);
+  };
+  previous_frame_time = SDL_GetTicks();
+  // --- Lock end ---//
+
+  cube_rotation.x += 0.01;
+  cube_rotation.y += 0.01;
+  cube_rotation.z += 0.01;
+
   for (int i = 0; i < N_POINTS; i++) {
     vec3_t point = cube_points[i];
 
-    // Move away from camera
-    point.z -= camera_position.z;
+    // Rotate the cube by x,y and z
+    vec3_t transformed_point = vec3_rotate_y(point, cube_rotation.y);
+    transformed_point = vec3_rotate_x(transformed_point, cube_rotation.x);
+    transformed_point = vec3_rotate_z(transformed_point, cube_rotation.z);
+
+    // Translate the point away from camera
+    transformed_point.z += camera_position.z;
+    transformed_point.x += camera_position.x;
+    transformed_point.y += camera_position.y;
 
     // 2D projected point
-    vec2_t projected_point = project(point);
+    vec2_t projected_point = project(transformed_point);
 
     // Save the projected 2D vector in the array of projected points
     projected_points[i] = projected_point;
